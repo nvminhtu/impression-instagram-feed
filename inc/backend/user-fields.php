4e6f62b66ce2ac8b1848e4400373fe0415b2bd57
Tu Nguyen <nvminhtu@gmail.com>
2017-02-16 17:24:37 +0700

    create and mod for own plugin - instagram

---
+++ inc/backend/user-fields.php
@@ -0,0 +1,61 @@
+<?php 
+add_action( 'show_user_profile', 'my_show_extra_profile_fields' );
+add_action( 'edit_user_profile', 'my_show_extra_profile_fields' );
+
+function my_show_extra_profile_fields( $user ) { ?>
+
+	<h3>Instagram Setup Information</h3>
+
+	<table class="form-table">
+
+		<tr>
+			<th><label for="insta_username">Instagram Username</label></th>
+
+			<td>
+				<input type="text" name="insta_username" id="insta_username" value="<?php echo esc_attr( get_the_author_meta( 'insta_username', $user->ID ) ); ?>" class="regular-text" /><br />
+				<span class="description">Please enter your Instagram username.</span>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="insta_acess_token">Instagram Access Token</label></th>
+			<td>
+				 <?php
+				 	//check value is existed or not
+				 	$insta_access_token = get_the_author_meta( 'insta_access_token', $user->ID );
+                    if(isset($_GET['access_token'])){
+                        $access_token = $_GET['access_token'];
+                    } else if(isset($insta_access_token) && $insta_access_token !=''){
+                        $access_token = $insta_access_token;
+                    } else{
+                        $access_token = '';
+                    }
+                ?>
+				<input type="text" name="insta_access_token" id="insta_access_token" value="<?php echo esc_attr($access_token); ?>" class="regular-text" /><br />
+				<?php
+					//get Admin URL
+					$uri = $_SERVER['REQUEST_URI'];
+					$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+					$admin_url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+					//mtcode: instagram link
+				?>
+				<?php /* <div id="login_with_instagram"><a href="https://api.instagram.com/oauth/authorize/?client_id=54da896cf80343ecb0e356ac5479d9ec&amp;scope=basic+public_content&amp;redirect_uri=http://api.web-dorado.com/instagram/?return_url=<?php echo $admin_url; ?>&response_type=token">Click to get Access Token</a><span class="description"> (instagram access token)</span>
+				</div> */?>
+			</td>
+		</tr>
+
+	</table>
+<?php } 
+
+add_action( 'personal_options_update', 'my_save_extra_profile_fields' );
+add_action( 'edit_user_profile_update', 'my_save_extra_profile_fields' );
+
+function my_save_extra_profile_fields( $user_id ) {
+
+	if ( !current_user_can( 'edit_user', $user_id ) )
+		return false;
+
+	update_usermeta( $user_id, 'insta_username', $_POST['insta_username'] );
+	update_usermeta( $user_id, 'insta_access_token', $_POST['insta_access_token'] );
+}
+
+?>
---
+++ inc/frontend/instagram-slider.php
@@ -0,0 +1,78 @@
+<?php
+defined( 'ABSPATH' ) or die( "No script kiddies please!" );
+global $apif_settings, $insta;
+    $apif_settings = get_option( 'apif_settings' );
+    // $username = $apif_settings['username']; // your username
+    $username = $set_username;
+    $access_token = $apif_settings['access_token'];
+    $image_like = $apif_settings['active'];
+    $count = 10; // number of images to show
+    require_once('instagram.php');
+    if($username == '' && $access_token ==''){
+        $response = array('meta'=>array('error_message'=>'Username and access token field is empty. Please configure.'));
+
+    }else if($username == ''){
+        $response = array('meta'=>array('error_message'=>'Username field is empty.'));
+
+    }else if ($access_token == ''){
+        $response = array('meta'=>array('error_message'=>'Access token field is empty.'));
+    }else{
+        $response = $insta->userMedia();
+    }
+
+    if($response == NULL){
+        $response = array('meta'=>array('error_message'=>'Username field is empty.'));
+    }
+
+    $ins_media_slider = $response;
+    ?>
+        <?php
+        $j = 0;
+        //print_r($response);
+        if(isset($ins_media_slider['meta']['error_message'])){
+            ?>
+               <h1 class="widget-title-insta"><span><?php echo $ins_media_slider['meta']['error_message']; ?></span></h1> 
+            <?php
+        } else if (is_array($ins_media_slider['data']) || is_object($ins_media_slider['data'])) { ?>
+                <div class="ct_article_box clearfix">
+                  <p class="ptitle_02">Instagramの写真</p>
+                  <div class="ct_insta_list_out clearfix">
+                    <div id="ct_insta_list" class="">
+                <?php
+                foreach ($ins_media_slider['data'] as $vm):
+                    if ($count == $j) {
+                        break;
+                    }
+                    $j++;
+                    //print_r($vm);
+                    $imgslider = $vm['images']['standard_resolution']['url'];
+                    $img_alt = $vm['caption']['text'];
+
+                    //print_r($vm['attribution']);
+                    //#mtcode
+
+                    $username = $vm['user']['username'];
+                    $profile_picture = $vm['user']['profile_picture'];
+                    $publish_date = $vm['created_time'];
+                    $caption_date = $vm['caption']['created_time'];
+                ?>
+              
+                        
+                        <div class="ct_insta_bx01">
+                            <img src="<?php echo esc_url($imgslider); ?>" alt='<?php echo esc_attr( $img_alt ); ?>'/>
+                            <?php echo $username; ?>
+                            <?php echo $date; ?>
+                            <?php echo date('M j, Y', $publish_date); ?>
+                            <?php echo $img_alt ?>
+                            <?php if ($image_like == '1') : ?>
+                               
+                                <!-- Image like cound section end -->
+                        </div>
+                    <?php endif; ?>
+                    
+
+                <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
